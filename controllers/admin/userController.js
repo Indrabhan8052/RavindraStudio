e7fdca01d6d258236cc396d8020cad32a459812d
Indra Bhan Verma <152368027+Indrabhan8052@users.noreply.github.com>
2026-09-13 01:17:18 +0530

    Fix import path for User model in userController

--- controllers/admin/userController.js
+++ controllers/admin/userController.js
@@ -1,5 +1,5 @@
 // controllers/userController.js
-const User = require('../models/User');
+const User = require('../../models/User');
 const Address = require('../models/Address');
 const bcrypt = require('bcryptjs');
 const { isProduction } = require('../config/cloudinary');
